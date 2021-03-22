52997ae9c3c6baed48142f2f1fd50defd866123f
HossamOkasha <enghossamokashaa@gmail.com>
2021-03-22 06:56:55 +0200

    add unit tests for 4 cases

---
+++ backend/tests/__init__.py
@@ -0,0 +1,54 @@
+import unittest
+import json
+from flaskr import create_app
+from flaskr.db import setup_db, db, Todo, TodoList 
+
+from os import environ
+
+
+class checkAllTestCase(unittest.TestCase):
+  def setUp(self):
+    self.app = create_app(True)
+    self.client = self.app.test_client
+    setup_db(self.app, environ.get("TEST_DATABASE_URI"), True)
+    self.new_list = {"name": 'new list20'}
+
+    self.new_todo = {"description": 'new todo'}
+    self.headersConf = {'Content-type': 'application/json','Access-Control-Allow-Origin': '*'}
+  
+    
+  def test_add_new_list(self):
+    res = self.client().post(
+      '/lists', headers=self.headersConf,
+      json=self.new_list)
+    data = json.loads(res.data)
+    self.assertEqual(res.status_code, 200)
+    self.assertEqual(data['success'], True)
+    self.assertTrue(data['created'])
+    self.assertTrue(data['totalLists'])
+
+  def test_add_new_todo(self):
+    res = self.client().post(
+      '/lists/15/todos', headers=self.headersConf,
+      json=self.new_todo)
+    data = json.loads(res.data)
+    self.assertEqual(res.status_code, 200)
+    self.assertEqual(data['success'], True)
+    self.assertTrue(data['created'])
+    
+  def test_get_lists(self):
+    res = self.client().get('/lists', headers=self.headersConf)
+    data = json.loads(res.data)
+
+    self.assertEqual(res.status_code, 200)
+    self.assertEqual(data['success'], True)
+    self.assertTrue(data['totalLists'])
+    self.assertTrue(data['allTodos'])
+
+  def test_404_get_todos_from_list(self):
+    res = self.client().get('/lists/1000/todos', headers=self.headersConf)
+    data = json.loads(res.data)
+
+    self.assertEqual(res.status_code, 404)
+    self.assertEqual(data['success'], False)
+    self.assertEqual(data['message'], 'Not Found!')
